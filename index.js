52fb1df2464ec3e13d4c740159d52a963197b0cf
Saurabh Yadav <saurabhkumar22410@gmail.com>
2026-06-28 14:06:12 +0530

    new

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 app.get("/", (req, res) => {
-  return res.status(200).json({ message: "hello duniya maine deploy kar diya hai project ko dekhlooo" })
+  return res.status(200).json({ message: "hello duniya maine deploy kar diya hai project ko dekhlooo guzzzzzzzz!!!!!" })
 });
 app.get("/health", (req, res) => {
   return res.status(200).json({ message: "goodddd hai bhaiya !!!" });
